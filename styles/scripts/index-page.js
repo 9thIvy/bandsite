5bfafa2a0fa121b34b277f4190db96c45cb648eb
9thIvy <mythril-pops@protonmail.com>
2024-11-24 20:19:15 -0800

    Styling fixes

--- styles/scripts/index-page.js
+++ styles/scripts/index-page.js
@@ -4,6 +4,46 @@ const bandSiteBlack = '#323232';
 const form = document.querySelector('.comment-section__form');
 const commentSection = document.querySelector('.comment-section')
 
+const makeApiKey = () => {
+    const set = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm';
+    const length = set.length;
+    let key = '';
+    for (let i = 0; i < length; i++) { 
+        key += set.charAt(Math.floor(Math.random() * length));
+    }
+
+    return key;
+};
+
+class BandSiteApi{
+    constructor(apiKey){
+        this.apiKey = apiKey;
+        this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/";
+    }
+
+    postComment(commentObject){
+        axios.post(this.baseUrl + 'comments' + '?api_key=' + this.apiKey, commentObject)
+        .then( (response) =>{
+            console.log(response);
+        })
+        .catch((error) =>{
+            console.log(error);
+        });
+        
+
+    }
+}
+
+
+/*
+let testKey = makeApiKey();
+let test = new BandSiteApi(testKey);
+test.postComment({
+    "name": "Cthulu Allspark",
+    "comment": "What a great band."
+});
+*/
+
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
